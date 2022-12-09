b04627251d2c8ccd768315ead9a32412e942646a
Klaudia Kepka <kepkaklaudia@gmail.com>
2022-12-10 00:55:58 +0100

    Current date display added

---
+++ src/Time/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+
+const Time = () => {
+
+	const [date, setDate] = useState(new Date());
+
+	const formattedDate = date.toLocaleString("pl", {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	});
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setDate(new Date());
+		}, 1000);
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, []);
+
+	return (
+		<div className="time__container">
+			<>Dzisiaj jest {formattedDate}</>
+		</div>
+	);
+};
+
+export default Time;
