d77a808ab95713fad81477df3f66852bb31f9358
Klaudia Kepka <kepkaklaudia@gmail.com>
2022-12-15 23:27:50 +0100

    formattedDate moved from useCurrentDate to index

--- src/Time/index.js
+++ src/Time/index.js
@@ -2,7 +2,15 @@ import { Clock } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
 const Time = () => {
-	const formattedDate = useCurrentDate();
+	const date = useCurrentDate();
+	const formattedDate = date.toLocaleString("pl", {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	});
 
 	return (
 		<Clock>
--- src/Time/useCurrentDate.js
+++ src/Time/useCurrentDate.js
@@ -3,15 +3,6 @@ import { useState, useEffect } from "react";
 export const useCurrentDate = () => {
 	const [date, setDate] = useState(new Date());
 
-	const formattedDate = date.toLocaleString("pl", {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		hour: "2-digit",
-		minute: "2-digit",
-		second: "2-digit",
-	});
-
 	useEffect(() => {
 		const intervalId = setInterval(() => {
 			setDate(new Date());
@@ -22,6 +13,5 @@ export const useCurrentDate = () => {
 		};
 	}, []);
 
-	return formattedDate;
+	return date;
 };
-
