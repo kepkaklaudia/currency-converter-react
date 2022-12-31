f646a00ba86980403b63323c926ffba3756f2287
Klaudia Kepka <kepkaklaudia@gmail.com>
2022-12-31 16:54:01 +0100

    currencies.js moved to useData.js

--- src/Form/useData.js
+++ src/Form/useData.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
 export const useData = () => {
-  const [rates, setRates] = useState();
+  const [rates, setRates] = useState([]);
   const [date, setDate] = useState();
+  const [short, setShort] = useState([]);
+  const [loading, setLoading] = useState("loading");
 
   useEffect(() => {
     const getData = async () => {
@@ -12,14 +14,17 @@ export const useData = () => {
           throw new Error(response.statusText);
         }
         const data = await response.json();
-        setRates(Object.keys(data.rates));
+        setShort(Object.keys(data.rates));
         setDate(data.date);
+        setRates(data.rates);
+        setLoading("success");
       } catch (error) {
-        console.error("Coś złego się stało!", error);
+        console.error("Ups... Coś złego się stało!🤨", error);
+        setLoading("error");
       }
     };
-    getData();
+    setTimeout(getData, 3000);
   }, []);
 
-  return { rates, date };
+  return { rates, date, short, loading };
 };
