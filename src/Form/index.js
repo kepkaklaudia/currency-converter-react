ef0908b6e38e09172391635d35f1d4ac97b3d99d
Klaudia Kepka <kepkaklaudia@gmail.com>
2022-12-31 17:21:03 +0100

    Error, Loading added, rates changed to short

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,16 +1,17 @@
-import { useData } from "../currencies.js";
+import { useData } from "./useData.js";
 import { useState } from "react";
 import { Fieldset, Legend, Text, Input, Select, Button, Paragraph, Strong } from "./styled";
+import { Error } from "./Error/index";
+import { Loading } from "./Loading/index";
 
 const Form = () => {
   const [amount, setAmount] = useState("0.00");
   const [currency, setCurrency] = useState("EUR");
   const [result, setResult] = useState("");
 
-  const {rates, date} = useData();
+  const { rates, date, short, loading } = useData();
 
   const onInputChange = ({ target }) => setAmount(target.value);
-
   const onSelectChange = ({ target }) => setCurrency(target.value);
 
   const calculateResult = (amount, currency) => {
@@ -22,56 +23,67 @@ const Form = () => {
     calculateResult(amount, currency);
   }
 
-
   return (
     <form onSubmit={onFormSubmit}>
-      <Fieldset>
-        <Legend>
-          Kalkulator walut
-        </Legend>
-        <p>
-          <label>
-            <Text>Kwota PLN*</Text>
-            <Input
-              type="number"
-              title="Kwota PLN do przeliczenia"
-              step="0.1"
-              min="0"
-              max="999999999999"
-              value={amount}
-              onChange={onInputChange}
-              required
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <Text>Wybierz walutę</Text>
-            <Select
-              title="Na jaką walutę przeliczyć PLN"
-              value={currency}
-              onChange={onSelectChange}
-            >
-              {rates.map((rates) => (
-                <option
-                  key={rates}
-                  value={rates}
-                >
-                  {rates}
-                </option>
-              ))};
-            </Select>
-          </label>
-        </p>
-        <p>
-          <Button>
-            Przelicz kwotę!
-          </Button>
-        </p>
-        <Paragraph condition>*Wartość wymagana</Paragraph>
-        <Paragraph amount>Nowa kwota wynosi:<Strong> {result}</Strong></Paragraph>
-        <Paragraph info>Kursy walut pobrano z Europejskiego Centralnego Banku w dniu {date}</Paragraph>
-      </Fieldset>
+      {loading === "loading" ? <Loading></Loading>
+        : loading === "error" ? <Error></Error>
+          : (<>
+            <Fieldset>
+              <Legend>
+                Kalkulator walut
+              </Legend>
+              <p>
+                <label>
+                  <Text>Kwota PLN*</Text>
+                  <Input
+                    type="number"
+                    title="Kwota PLN do przeliczenia"
+                    step="0.01"
+                    min="0"
+                    max="999999999999"
+                    value={amount}
+                    onChange={onInputChange}
+                    required
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  <Text>Wybierz walutę</Text>
+                  <Select
+                    title="Na jaką walutę przeliczyć PLN"
+                    value={currency}
+                    onChange={onSelectChange}
+                  >
+                    {short.map((short) => (
+                      <option
+                        key={short}
+                        value={short}
+                      >
+                        {short}
+                      </option>
+                    ))};
+                  </Select>
+                </label>
+              </p>
+              <p>
+                <Button>
+                  Przelicz kwotę!
+                </Button>
+              </p>
+              <Paragraph condition>
+                *Wartość wymagana
+              </Paragraph>
+              <Paragraph amount>
+                Nowa kwota wynosi:
+                <Strong> {result}</Strong>
+              </Paragraph>
+              <Paragraph info>
+                Kursy walut pobrano z Europejskiego Centralnego Banku w dniu {date}
+              </Paragraph>
+            </Fieldset>
+          </>)
+      }
     </form>
   )
 };
