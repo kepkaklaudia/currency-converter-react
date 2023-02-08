368a20aed149e352c196f2027d7cd27e7744098d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-08 22:46:14 +0100

    refactoring

--- src/Form/Error/index.js
+++ src/Form/Error/index.js
@@ -1,13 +1,15 @@
 import { Paragraph, MoneyIcon } from "./styled";
 
-export const Error = () => {
-  return (
-    <><Paragraph>
+export const Error = () => (
+  <>
+    <Paragraph>
       Ups... Coś poszło nie tak!😳 <br />
       Czy masz połączenie z&nbsp;internetem?👩‍💻 <br />
       Jeśli tak, to pewnie nasza wina. <br />
       Proszę spróbować później.
-      <MoneyIcon>💸</MoneyIcon>
-    </Paragraph></>
-  )
-};
+      <MoneyIcon>
+        💸
+      </MoneyIcon>
+    </Paragraph>
+  </>
+);
