2ad4805f81004ac4aff63757a5708bdc54935e76
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-08 22:46:22 +0100

    refactoring

--- src/Form/Loading/index.js
+++ src/Form/Loading/index.js
@@ -1,12 +1,11 @@
 import { Paragraph, Loader } from "./styled";
 
-export const Loading = () => {
-  return (
-    <><Loader></Loader>
-      <Paragraph>
-        ⏱ Chwileczkę ⏱<br />
-        Trwa ładowanie danych z&nbsp;Europejskiego Banku Centralnego.
-      </Paragraph>
-    </>
-  )
-};
+export const Loading = () => (
+  <>
+    <Loader />
+    <Paragraph>
+      ⏱ Chwileczkę ⏱<br />
+      Trwa ładowanie danych z&nbsp;Europejskiego Banku Centralnego.
+    </Paragraph>
+  </>
+);
